296b7ebba5709869e327525bccd824c01c66ba7d
твое_имя <e1mirbek>
2025-05-12 13:48:45 +0600

    #oop-encapsulation: Add encapsulation example for OOP

--- src/oop/encapsulation/ClassAndObjects.java
+++ src/oop/encapsulation/ClassAndObjects.java
@@ -1,7 +1,13 @@
 package oop.encapsulation;
 
+// Class - это шаблон для создание обьектов
+
+// Поля (переменные класса) - данные, которые будут храниться в объекте
+
+// Методы - действия, которые может выполнять объект
+
 public class ClassAndObjects {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Класс и обьекты");
     }
 }
