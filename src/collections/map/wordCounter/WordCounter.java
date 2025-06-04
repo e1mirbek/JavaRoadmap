3f7e0c8ece2363222348736d8b2bbba0bed3bbb0
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-04 17:53:39 +0600

    #learn: added word counter app with map

--- src/collections/map/wordCounter/WordCounter.java
+++ src/collections/map/wordCounter/WordCounter.java
@@ -3,6 +3,11 @@ package collections.map.wordCounter;
 import java.util.HashMap;
 import java.util.Map;
 
+// Программа предназначена для подсчёта количества вхождений
+// каждого слова в заданном тексте.
+// Она демонстрирует, как можно использовать отображения (Map)
+// для анализа текстовых данных и подсчёта частоты повторения слов.
+
 public class WordCounter {
     public static void main(String[] args) {
         String text = "Java легко освоить, Java — это мощный инструмент";
@@ -14,7 +19,6 @@ public class WordCounter {
         System.out.println("Частоты слов : ");
         for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
             System.out.println(entry.getKey() + entry.getValue());
-
         }
     }
 }
