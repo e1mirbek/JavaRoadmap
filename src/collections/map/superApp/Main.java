36183e5fcec36e6a8e17be281b3db3e48110f335
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-07 13:26:02 +0600

    #learn: The main program in which all classes of the program are called

--- src/collections/map/superApp/Main.java
+++ src/collections/map/superApp/Main.java
@@ -1,7 +1,25 @@
 package collections.map.superApp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        WordCounter wordCounter = new WordCounter();
+        StudentGrades studentGrades = new StudentGrades();
+
+        while (true) {
+            System.out.println("\\n=== МЕНЮ ===");
+            System.out.println("1. Подсчет слов ");
+            System.out.println("2. Оценка студентов ");
+            System.out.printf("Ваш выбор : ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1 -> wordCounter.countWords(scanner);
+                case 2 -> studentGrades.run(scanner);
+            }
+        }
     }
 }
