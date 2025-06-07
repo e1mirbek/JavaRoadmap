fbe992d48ccad489873039213a2c4b5d89ec0576
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-07 13:02:57 +0600

    #learn: word Counter class with map

--- src/collections/map/superApp/WordCounter.java
+++ src/collections/map/superApp/WordCounter.java
@@ -1,4 +1,22 @@
 package collections.map.superApp;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class WordCounter {
+    public  void countWords (Scanner scanner) {
+        System.out.println("Введите текст : ");
+        String text = scanner.nextLine();
+        String[] words = text.split(" ");
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : words) {
+            word = word.toLowerCase();
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+        System.out.println("Частота слов : ");
+        wordCount.forEach((key, value) -> System.out.println(key + " " + value));
+
+    }
 }
