f5b349d6b21b17de2961b8af6fa427733aeb441f
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-09 18:16:34 +0600

    #learn: The main program in which all classes of the program are called

--- src/collections/map/superApp/Main.java
+++ src/collections/map/superApp/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        PhoneBook phoneBook = new PhoneBook();
+        InventoryManager inventoryManager = new InventoryManager();
         WordCounter wordCounter = new WordCounter();
         StudentGrades studentGrades = new StudentGrades();
 
@@ -12,13 +14,17 @@ public class Main {
             System.out.println("\\n=== МЕНЮ ===");
             System.out.println("1. Подсчет слов ");
             System.out.println("2. Оценка студентов ");
+            System.out.println("3. Телефон номер");
+            System.out.println("4. Магазин");
             System.out.printf("Ваш выбор : ");
             int choice = scanner.nextInt();
             scanner.nextLine();
-
             switch (choice) {
                 case 1 -> wordCounter.countWords(scanner);
                 case 2 -> studentGrades.run(scanner);
+                case 3 -> phoneBook.run(scanner);
+                case 4 -> inventoryManager.run(scanner);
+                default -> System.out.println("Неверный выбор");
             }
         }
     }
