3d0703db35864ad53a426d2359971e0bfc6a8377
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-14 13:13:01 +0600

    #learn: Added example of handling ArrayIndexOutOfBoundsException when accessing an invalid array index

---
+++ src/exceptions/arrayGoingBeyondBoundaries/ArrayExample.java
@@ -0,0 +1,16 @@
+package exceptions.arrayGoingBeyondBoundaries;
+
+
+
+
+// Массив и выход за границы
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3};
+        try{
+            System.out.println(numbers[5]); // нет такого индекса
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Ошибка: Индекс вне диапазона массива");
+        }
+    }
+}
