66355b2348b0b23518290a6b4db0e87f9166ed56
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-21 17:59:55 +0600

    Добавлен простой счётчик без синхронизации

---
+++ src/multithreading/tasks/counter/SimpleCounter.java
@@ -0,0 +1,19 @@
+package multithreading.tasks.counter;
+
+public class SimpleCounter {
+    private int count;
+
+    public SimpleCounter () {
+        this.count = 0;
+    }
+
+    public void increment () {
+        count++;
+    }
+
+    public int get () {
+        return count;
+    }
+
+
+}
