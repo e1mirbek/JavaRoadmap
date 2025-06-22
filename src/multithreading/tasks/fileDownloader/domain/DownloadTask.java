ff7c25d7f55617e39d106a03cf304e6179affd58
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-22 15:12:16 +0600

    feat: добавлен класс DownloadTask для представления задачи загрузки файла
    - Содержит поле fileName с геттером
    - Используется для хранения информации о загружаемом файле

--- src/multithreading/tasks/fileDownloader/domain/DownloadTask.java
+++ src/multithreading/tasks/fileDownloader/domain/DownloadTask.java
@@ -1,4 +1,14 @@
 package multithreading.tasks.fileDownloader.domain;
 
 public class DownloadTask {
+    private final String fileName;
+
+    public DownloadTask(String fileName) {
+        this.fileName = fileName;
+    }
+
+
+    public String getFileName() {
+        return fileName;
+    }
 }
