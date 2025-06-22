7f99d08ff6b1f9acde1ffb2438597d45293f9e31
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-22 15:11:18 +0600

    feat: реализован DownloadController для запуска загрузки файлов через usecase
    - Создан метод start(), инициализирующий FakeDownloader и Downloadusecase
    - Формируется список задач загрузки (DownloadTask)
    - Запускается многопоточная загрузка через usecase.downloadFiles()

--- src/multithreading/tasks/fileDownloader/controller/DownloadController.java
+++ src/multithreading/tasks/fileDownloader/controller/DownloadController.java
@@ -1,4 +1,24 @@
 package multithreading.tasks.fileDownloader.controller;
 
+import multithreading.tasks.fileDownloader.domain.DownloadTask;
+import multithreading.tasks.fileDownloader.infrastructure.FakeDownloader;
+import multithreading.tasks.fileDownloader.usecase.Downloadusecase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DownloadController {
+    public void start () {
+        var downloader = new FakeDownloader();
+        var usecase = new Downloadusecase(downloader);
+        List <DownloadTask> tasks = Arrays.asList(
+                new DownloadTask("Файл_1.txt"),
+                new DownloadTask("Файл_2.txt"),
+                new DownloadTask("Файл_3.txt"),
+                new DownloadTask("Файл_4.txt"),
+                new DownloadTask("Файл_5.txt")
+                );
+        usecase.downloadFiles(tasks);
+    }
 }
