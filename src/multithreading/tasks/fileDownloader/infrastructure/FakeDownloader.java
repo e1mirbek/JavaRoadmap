693f88d3480d24192c9ca8906b3b5836626ef0f3
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-22 15:13:13 +0600

    feat: добавлен FakeDownloader для имитации процесса загрузки файла
    - Реализует интерфейс Downloader
    - Имитирует загрузку через Thread.sleep(2000)
    - Выводит в консоль начало и завершение загрузки с именем потока

--- src/multithreading/tasks/fileDownloader/infrastructure/FakeDownloader.java
+++ src/multithreading/tasks/fileDownloader/infrastructure/FakeDownloader.java
@@ -1,4 +1,21 @@
 package multithreading.tasks.fileDownloader.infrastructure;
 
-public class FakeDownloader {
+
+// просто ждёт и выводит текст
+
+import multithreading.tasks.fileDownloader.domain.DownloadTask;
+import multithreading.tasks.fileDownloader.domain.Downloader;
+
+public class FakeDownloader implements Downloader {
+
+    @Override
+    public void download(DownloadTask task) {
+        System.out.println(Thread.currentThread().getName() + "Начинает скачивание" + task.getFileName());
+        try {
+            Thread.sleep(2000); // // имитация загрузки 2 секунды
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName() + "Завершил скачивание" + task.getFileName());
+    }
 }
