48cdb5a9a6e76fe2998adb2a3bb1ceb48e1a7d1e
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-06-26 21:55:48 +0600

    feat: реализована демонстрация аннотаций и рефлексии в Java
    - Добавлена пользовательская аннотация @MyAnnotation с параметром value
    - Создан класс Test с методом, помеченным аннотацией
    - Добавлен класс Main, использующий рефлексию для поиска методов с аннотацией
    - При нахождении аннотированного метода выводится значение аннотации и вызывается метод

---
+++ src/annotationsAndReflections/annotation/Main.java
@@ -0,0 +1,51 @@
+package annotationsAndReflections.annotation;
+
+
+// Что такое аннотации
+
+// Аннотации это метки
+// Они не влияют напрямую на выполнение кода,
+// но могут использоваться инструментами, компилятором или во время выполнения программы.
+
+// | Аннотация           | Назначение                                             |
+//| ------------------- | ------------------------------------------------------ |
+//| `@Override`         | Показывает, что метод переопределяет метод суперкласса |
+//| `@Deprecated`       | Помечает метод устаревшим                              |
+//| `@SuppressWarnings` | Подавляет предупреждения компилятора                   |
+
+// Что такое рефлексия (Reflection)
+
+// Рефлексия — это механизм,
+// позволяющий исследовать и изменять структуру классов во время выполнения программы.
+// Например: получить имя класса, список методов, аннотации, вызвать методы и т.д.
+
+
+import annotationsAndReflections.testApp.Test;
+
+import java.lang.reflect.Method;
+
+// Пример получения информации через рефлексию:
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Class <?> clazz = Test.class; // получаем обьект класса
+
+        // clazz.getDeclaredMethods() — возвращает все методы, объявленные в классе Test
+        for (Method method : clazz.getDeclaredMethods()) {
+
+            // ищем методы с аннотацией MyAnnotation
+            if (method.isAnnotationPresent(MyAnnotation.class)){
+
+                // получаем объект аннотации
+                MyAnnotation a = method.getAnnotation(MyAnnotation.class);
+
+                // выводим значение из аннотации
+                System.out.println("Найден метод с аннотацией" + a.value());
+
+                // вызываем метод (sayHello()) у нового объекта Test
+                method.invoke(new MyTest());
+            }
+        }
+
+    }
+}
+
