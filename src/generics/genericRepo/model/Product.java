74bd172a46e1dc297b8b61786476d58c6bb5518d
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-07-30 20:49:00 +0600

    feat(model): добавлена модель Product с полями id, name и price, реализующая интерфейс Entity

---
+++ src/generics/genericRepo/model/Product.java
@@ -0,0 +1,39 @@
+package generics.genericRepo.model;
+
+
+
+// Модель данных
+
+public class Product implements Entity {
+    private Long id;
+    private String name;
+    private double price;
+
+    public Product(Long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double isPrice() {
+        return price;
+    }
+
+
+
+
+}
