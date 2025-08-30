14af458e2db2e4a47ee69fe272ef85daf6a29bcb
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-08-30 15:17:46 +0600

    #learn: 2 lesson course udemy (print, println)

---
+++ src/udemy/course/lesson3/ThirdLs.java
@@ -0,0 +1,13 @@
+package udemy.course.lesson3;
+
+
+// понимание работы с print и println
+
+public class ThirdLs {
+    void main () {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+    }
+}
