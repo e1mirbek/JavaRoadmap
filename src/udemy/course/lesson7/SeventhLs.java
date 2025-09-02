109684103d202d0ffef3b73508c3b3d999ecbb13
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-09-02 20:16:54 +0600

    #learn: 7 lesson course udemy (примитивные тип данных)

---
+++ src/udemy/course/lesson7/SeventhLs.java
@@ -0,0 +1,26 @@
+package udemy.course.lesson7;
+
+// примитивные типы данных
+
+public class SeventhLs {
+    public static void main(String[] args) {
+
+        // ссылочный тип данных
+
+        String name = "Elmirbek";
+
+        // примитивный тип данных
+
+        int age = 19;
+
+        long numbers =  10000000000L;
+
+        float floatNumber = 55.9F;
+
+        double doubleNumber = 55.9;
+
+
+
+
+    }
+}
