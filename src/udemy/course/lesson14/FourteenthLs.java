fbc6ed22e6e9cd7da6097438186a479dcbe2069a
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-09-06 20:46:34 +0600

    #learn: 14 lesson course udemy (var - вывод типов type interface)

---
+++ src/udemy/course/lesson14/FourteenthLs.java
@@ -0,0 +1,15 @@
+package udemy.course.lesson14;
+
+// var - вывод типов (type - интерфейс) - компилятор сам определяет тип переменной
+
+// ❗ var - Тип определяется один раз при компиляции и дальше фиксируется.
+
+public class FourteenthLs {
+    public static void main(String[] args) {
+
+        var name = "Elmirbek";
+
+        var age = 19;
+
+    }
+}
