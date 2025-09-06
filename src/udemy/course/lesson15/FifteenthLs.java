a0e31b3a37e37f65468bb5a62a02968ef29528cd
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-09-06 21:06:24 +0600

    #learn: 14 lesson course udemy (Конкатенация строк)

---
+++ src/udemy/course/lesson15/FifteenthLs.java
@@ -0,0 +1,35 @@
+package udemy.course.lesson15;
+
+// Конкатенация строк — это просто склеивание (соединение) строк в одну.
+
+public class FifteenthLs {
+
+    public static void main(String[] args) {
+
+        String firstName = "Elmirbek";
+        String lastName = "Abdumanapov";
+
+        int age = 19;
+
+        String userInformation = "Elmirbek " + "Abdumanapov " +  19;
+
+        System.out.println(userInformation);
+
+        FifteenthLs user = new FifteenthLs();
+        user.userInformations("Elmirbek", "Abdumanapov", 19);
+
+
+    }
+
+    void userInformations (String firstName, String lastName, int age) {
+
+        System.out.println("Имя : " + firstName);
+        System.out.println("Фамилия : " + lastName);
+        System.out.println("Возраст : " + age);
+
+    }
+
+}
+
+
+
