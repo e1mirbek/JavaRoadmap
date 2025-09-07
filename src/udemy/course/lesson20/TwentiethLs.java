ecbf907a0ec414457fa953d133b5a31b32b0b54f
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-09-07 14:49:08 +0600

    #learn: 20 lesson course udemy (оператор !=)

---
+++ src/udemy/course/lesson20/TwentiethLs.java
@@ -0,0 +1,17 @@
+package udemy.course.lesson20;
+
+// оператор !=
+
+public class TwentiethLs {
+
+    public static void main(String[] args) {
+
+        double delimoe = 10.0;
+        double delitel = 10.0;
+
+      boolean result =  delitel != 0 && delimoe != 0;
+        System.out.println(result);
+
+    }
+
+}
