a51b6b933f04a3737664ecfb5a15386b432e5d20
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-09-10 19:51:09 +0600

    #learn: 21 lesson course udemy (if - then : стейтмент)

---
+++ src/udemy/course/lesson21/TwentyFirstLs.java
@@ -0,0 +1,18 @@
+package udemy.course.lesson21;
+
+// if - then : стейтмент
+
+public class TwentyFirstLs {
+    public static void main(String[] args) {
+
+        int delimoe = 5;
+        int delitel = 0;
+        if (delitel != 0) {
+            // выражение
+            int result = delimoe / delitel;
+            System.out.println(result);
+        } else {
+            System.out.println("Ошибка delitel равен 0");
+        }
+    }
+}
