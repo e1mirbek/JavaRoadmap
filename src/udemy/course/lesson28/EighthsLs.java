2b787d30752156d56ae41fed4b4bd58ed26a3081
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-09-15 21:03:06 +0600

    #learn: 28 lesson course udemy (Цикл - for)

---
+++ src/udemy/course/lesson28/EighthsLs.java
@@ -0,0 +1,13 @@
+package udemy.course.lesson28;
+
+// Цикл - for
+
+public class EighthsLs {
+    public static void main(String[] args) {
+        int givenTimes = 10;
+        for (int i = 0; i < givenTimes; i++) {
+            System.out.println(i);
+        }
+        System.out.println("outside");
+    }
+}
