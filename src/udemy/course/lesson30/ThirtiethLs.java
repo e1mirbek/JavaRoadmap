abb8bc4f7bb2ae7f4a823fad7872c45f4a55f29d
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-09-29 14:31:51 +0600

    #learn: 30 lesson course udemy (Оброботка исключений)

---
+++ src/udemy/course/lesson30/ThirtiethLs.java
@@ -0,0 +1,20 @@
+package udemy.course.lesson30;
+
+// Оброботка исключений
+
+public class ThirtiethLs {
+    public static void main(String[] args) {
+
+        int num = parseInt("hello");
+
+        int number = Integer.parseInt("100");
+        System.out.println(number);
+
+
+
+    }
+
+    public static int parseInt (String str) {
+        return Integer.parseInt(str);
+    }
+}
