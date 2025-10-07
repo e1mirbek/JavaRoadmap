15a686fd7c5cb9f7972678815a047fa0f73377ad
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-10-07 20:09:20 +0600

    Add lesson32: demonstrate String.join() method and int to String conversion

--- src/udemy/course/lesson32/ThirtySecondLs.java
+++ src/udemy/course/lesson32/ThirtySecondLs.java
@@ -1,4 +1,22 @@
 package udemy.course.lesson32;
 
+
+// Метод String join 
+
 public class ThirtySecondLs {
+
+    public static void main(String[] args) {
+        String fistName = "Elmirbek";
+        String lastName = "Abdumanapov";
+
+        int age = 20;
+
+        String info = String.join(" ", fistName, lastName, String.valueOf(age));
+
+        System.out.println(info);
+
+
+
+
+    }
 }
