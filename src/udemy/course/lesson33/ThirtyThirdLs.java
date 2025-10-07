f1df79caaabb31a1c7238a8661f864cd123aa13c
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-10-07 20:19:46 +0600

    Add lesson33: demonstrate String immutability and toUpperCase() usage

---
+++ src/udemy/course/lesson33/ThirtyThirdLs.java
@@ -0,0 +1,18 @@
+package udemy.course.lesson33;
+
+// Неизменямость строк (String Immutability)
+
+public class ThirtyThirdLs {
+
+    public static void main(String[] args) {
+
+       String name = "Elmirbek";
+
+      name = name.toUpperCase();
+
+      System.out.println(name);
+
+
+        
+    }
+}
